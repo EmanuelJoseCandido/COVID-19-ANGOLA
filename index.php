247853adf3112fd94020e4af903151c2ca4cfed9
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2020-05-02 04:32:27 +0100

    Organização das pastas e criação do index.php.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!-- 
+	Autor: Emanuel Cândido 
+	Email: emanueljosecandido@hotmail.com
+	GitHub: https://github.com/EmanuelJoseCandido
+	WebSite: COVID-19 em Angola.
+	WebSite escolhido para fazer a recolha de dados: https://www.worldometers.info/coronavirus/
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="css/style.css">
+		<title>COVID-19 EM ANGOLA</title>
+	</head>
+	<body>
+		<header>
+		</header>
+		<main>
+			<section>
+				<article>
+				</article>
+			</section>
+			<aside>
+				<article>
+				</article>
+			</aside>
+		</main>
+	</body>
+	 <footer>
+		<p>Todos os direitos reservados <br> Copyright &copy; 2020 - by <a href="http://facebook.com/EmanuelCandido">Emanuel Cândido</a></p>
+	</footer>
+	<script type="text/javascript" src="js/main.js"></script>
+</html>
